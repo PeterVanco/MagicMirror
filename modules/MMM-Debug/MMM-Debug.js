83207c56b7c4f45fd660e29a4034655d3f390106
Peter Vanco <petervanco@petervanco.sk>
2018-03-08 09:40:55 +0100

    Mostly styling + refactoring

--- modules/MMM-Debug/MMM-Debug.js
+++ modules/MMM-Debug/MMM-Debug.js
@@ -1,13 +1,3 @@
-/* global Module Log */
-
-/* Magic Mirror
- * Module: MMM-TTS
- *
- * By fewieden https://github.com/fewieden/MMM-TTS
- *
- * MIT Licensed.
- */
-
 Module.register('MMM-Debug', {
 
     defaults: {
@@ -17,6 +7,10 @@ Module.register('MMM-Debug', {
     start() {
         Log.info(`Starting module: ${this.name}`);
         this.sendNotification('DEBUG', this.config.debug);
+    },
+
+    getStyles: function() {
+        return ["MMM-Debug.css"];
     },
 
     notificationReceived(notification, payload) {
@@ -35,18 +29,65 @@ Module.register('MMM-Debug', {
         const self = this;
         const wrapper = document.createElement('div');
         if (this.config.debug === true) {
-            wrapper.innerHTML =
-                '<input id="debug-notification" type="text" placeholder="notification">' +
-                '<textarea id="debug-payload" cols="60" rows="5" placeholder="payload"></textarea>';
+
+
+            const notificationTypeWrapper = document.createElement('div');
+            const localNotification = document.createElement('input');
+            localNotification.id = 'local';
+            localNotification.type = 'radio';
+            localNotification.name = "notificationType";
+            localNotification.checked = true;
+            const localNotificationLabel = document.createElement('label');
+            localNotificationLabel.setAttribute("for", "local");
+            localNotificationLabel.innerText = "Local";
+            notificationTypeWrapper.appendChild(localNotification);
+            notificationTypeWrapper.appendChild(localNotificationLabel);
+
+            const socketNotification = document.createElement('input');
+            socketNotification.id = 'socket';
+            socketNotification.type = 'radio';
+            socketNotification.name = "notificationType";
+            const socketNotificationLabel = document.createElement('label');
+            socketNotificationLabel.setAttribute("for", 'socket');
+            socketNotificationLabel.innerText = 'Socket';
+            notificationTypeWrapper.appendChild(socketNotification);
+            notificationTypeWrapper.appendChild(socketNotificationLabel);
+
+            wrapper.appendChild(notificationTypeWrapper);
+
+            const notification = document.createElement("input");
+            notification.id = "debug-notification";
+            notification.type = "text";
+            notification.placeholder = "notification";
+            wrapper.appendChild(notification);
+
             const button = document.createElement("button");
+            button.id = "debug-send-button";
             button.innerText = "SEND";
-            button.onclick = () => {
+            button.onclick = function() {
                 let notification = document.getElementById("debug-notification").value;
-                let payload = document.getElementById("debug-payload").value;
-                console.log("Sending DEBUG SOCKET notification " + notification + ": " + JSON.stringify(payload));
-                self.sendNotification(notification, payload);
+                let payload;
+                try {
+                    payload = JSON.parse(document.getElementById("debug-payload").value);
+                } catch (e) {
+                    payload = document.getElementById("debug-payload").value;
+                }
+
+                if (localNotification.checked === true) {
+                    console.log("Sending LOCAL notification " + notification + ": " + JSON.stringify(payload));
+                    self.sendNotification(notification, payload);
+                } else {
+                    console.log("Sending SOCKET notification " + notification + ": " + JSON.stringify(payload));
+                    self.sendSocketNotification(notification, payload);
+                }
             };
             wrapper.appendChild(button);
+
+            const payload = document.createElement("textarea");
+            payload.id = "debug-payload";
+            payload.rows = 5;
+            payload.placeholder = "payload";
+            wrapper.appendChild(payload);
         }
         return wrapper;
     }
